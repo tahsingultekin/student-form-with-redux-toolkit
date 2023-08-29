a0892c6b6703b51e8ea0f15faf1f84628d453b68
tahsingultekin <112646842+tahsingultekin@users.noreply.github.com>
2023-08-29 18:37:02 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import Students from "./pages/Students";
-import Login from "./pages/Login";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 
@@ -7,8 +6,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/students" element={<Students />} />
       </Routes>
     </BrowserRouter>
